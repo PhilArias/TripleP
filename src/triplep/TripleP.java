daa208f91e069b2edd753bf52d97462c8f79c05e
FranZixX <FranZixX@10.203.187.111>
2022-05-12 17:09:24 -0500

    Comentario Hola

--- src/triplep/TripleP.java
+++ src/triplep/TripleP.java
@@ -14,7 +14,10 @@ public class TripleP {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        //hola
+        
+        
     }
     
 }
