f8b3670beac4b20835193d1d827e59a4fbb1a74b
felip <felip@PHILAPTOP-ASUS14>
2022-05-13 10:57:42 -0500

    Version 28

--- src/triplepp/TriplePp.java
+++ src/triplepp/TriplePp.java
@@ -15,7 +15,7 @@ public class TriplePp {
      */
     public static void main(String[] args) {
         // TODO code applica
-        //Version 15444222 dsdssds 
+        //Version 15444222 dsdssdswewew 
     }
     
 }
