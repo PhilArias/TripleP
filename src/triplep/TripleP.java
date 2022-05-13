60ea5f029682a089440c27783307083abb0586bd
felip <felip@PHILAPTOP-ASUS14>
2022-05-13 10:25:28 -0500

    Version 77

--- src/triplep/TripleP.java
+++ src/triplep/TripleP.java
@@ -17,7 +17,8 @@ public class TripleP {
         
         //UwU
         //Modificación #67
-        //No me joda Felipe, qué fastidio :v xD
+        // xD
+        //fff
         
         
     }
